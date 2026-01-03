fc3b40831b33ce1af42cec0a8fa48af8291b4286
Mihir Kumar <mihir2726@gmail.com>
2026-01-03 21:12:53 +0530

    removed stock

--- app.py
+++ app.py
@@ -18,39 +18,7 @@ def debug_data():
     response = yf.screen(q, sortField = 'intradaymarketcap', sortAsc = False, size=100)
     return jsonify(response)
 
-@app.route("/stocks")
-def stocks():
-    High_q = EquityQuery('and', [
-        EquityQuery('eq', ['region', 'in']),
-        EquityQuery('gt', ['intradaymarketcap', 1000000000])
-    ])
-    response = yf.screen(High_q, sortField='intradaymarketcap', sortAsc=False, size=100)
-    
-    stock_list = response.get('quotes', [])
-    
 
-    # Mid_q = EquityQuery('and', [
-    #     EquityQuery('eq', ['region', 'in']),
-    #     EquityQuery('gt', ['intradaymarketcap',600000000]),
-    #     EquityQuery('lt', ['intradaymarketcap', 2500000000])
-    # ])
-    # response2 = yf.screen(Mid_q, sortField='intradaymarketcap', size=250)
-    # stock_list_mid = response2.get('quotes',[])
-    
-    # Small_q = EquityQuery(
-    # 'and', [
-    #     EquityQuery('eq', ['region', 'in']),
-    #     EquityQuery('lt', ['intradaymarketcap', 600_000_000])
-    # ])
-
-    # response3 = yf.screen(
-    #     Small_q,
-    #     sortField='intradaymarketcap',
-    #     sortAsc=False,
-    #     size=250
-    # )
-
-    # stock_list_small = response3.get('quotes', [])
     
     return render_template('stocks.html', stocks=stock_list)
     
