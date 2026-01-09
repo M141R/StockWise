0f7f7188c6cedc23692e6c96923d8819e8c22207
Mihir Kumar <mihir2726@gmail.com>
2026-01-09 22:12:23 +0530

    Made changes for production

--- app.py
+++ app.py
@@ -14,31 +14,7 @@ def hello_world():
         print(user_budget)
         result = stock_analyzer(user_budget)
     return render_template('home.html',recommendations=result)
-
-@app.route("/debug")
-def debug_data():
-    q = EquityQuery('and', [
-    #    EquityQuery('gt', ['percentchange', 3]),
-       EquityQuery('eq', ['region', 'in']),
-        EquityQuery('gt', ['intradaymarketcap', 1000000000])
-])
-    response = yf.screen(q, sortField = 'intradaymarketcap', sortAsc = False, size=100)
-    return jsonify(response)
-
-@app.route("/stocks")
-def stocks():
-    High_q = EquityQuery('and', [
-        EquityQuery('eq', ['region', 'in']),
-        EquityQuery('gt', ['intradaymarketcap', 1000000000])
-    ])
-    response = yf.screen(High_q, sortField='intradaymarketcap', sortAsc=False, size=100)
-    
-    stock_list = response.get('quotes', [])
-    
-    return render_template('stocks.html', stocks=stock_list)
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-#hgg
+    app.run(debug=False,host='0.0.0.0')
